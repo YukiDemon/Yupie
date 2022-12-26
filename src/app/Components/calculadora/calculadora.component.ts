0960bf677d52f04bc6ba82dc94437d29c5470d4d
Guillermo Antonio Campos Vásquez <guillermo.camposwork@gmail.com>
2022-12-26 10:38:14 -0500

    Cambios 26/12/2022

--- src/app/Components/calculadora/calculadora.component.ts
+++ src/app/Components/calculadora/calculadora.component.ts
@@ -9,6 +9,9 @@ export class CalculadoraComponent {
   number1= '';
   number2= '';
   result:number = 0;
+  Ocult : boolean = false;
+  spanOcult1 : boolean = false;
+  spanOcult2 : boolean = false;
 
   A(event: Event){
     this.number1 = (<HTMLInputElement>event.target).value
@@ -19,10 +22,38 @@ export class CalculadoraComponent {
   }
 
 
-  Operation(){
-    this.result = parseInt(this.number1) + parseInt(this.number2)
-    console.log()
+  Suma(){
+      this.result = parseInt(this.number1) + parseInt(this.number2)
+      this.Ocult = !this.Ocult
   }
 
+  Resta(){
+      this.result = parseInt(this.number1) - parseInt(this.number2)
+      this.Ocult = !this.Ocult
+  }
+
+  Multiplicar(){
+      this.result = parseInt(this.number1) * parseInt(this.number2)
+      this.Ocult = !this.Ocult
+  }
+
+  Dividir(){
+      this.result = parseInt(this.number1) / parseInt(this.number2)
+      this.Ocult = !this.Ocult
+  }
+
+  SpanOcult(){
+    if(this.number1 == ''){
+      this.spanOcult1 = !this.spanOcult1
+    }else {
+      this.spanOcult1  == false;
+    }
+    
+    if(this.number2 == ''){
+      this.spanOcult2 = !this.spanOcult2
+    }else{
+      this.spanOcult2  == false;
+    }
+  }
   
 }
